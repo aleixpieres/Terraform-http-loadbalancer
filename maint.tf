16de23cf7b160d95b6a4ed8c584882c5227e2317
Aleix Pieres <55714906+aleixpieres@users.noreply.github.com>
2024-06-18 12:19:10 +0200

    Update maint.tf

--- maint.tf
+++ maint.tf
@@ -1,1 +1,115 @@
+#### SUBNETWORK
+#creating subnet 1 for mig group 1
+resource "google_compute_subnetwork" "group1" {
+  name                     = "${var.network_prefix}-group1"
+  ip_cidr_range            = "10.126.0.0/20"
+  network                  = google_compute_network.default.self_link
+  region                   = var.group1_region
+  private_ip_google_access = true
+}
 
+#### CLOUD ROUTER
+# Router and Cloud NAT are required for installing packages from repos (apache, php etc)
+resource "google_compute_router" "group1" {
+  name    = "${var.network_prefix}-gw-group1"
+  network = google_compute_network.default.self_link
+  region  = var.group1_region
+}
+
+#### CLOUD NAT
+module "cloud-nat-group1" {
+  source     = "terraform-google-modules/cloud-nat/google"
+  version    = "~> 5.0"
+  router     = google_compute_router.group1.name
+  project_id = var.project
+  region     = var.group1_region
+  name       = "${var.network_prefix}-cloud-nat-group1"
+}
+
+#### SUBNETWORK
+#creating subnet 2 for mig group 2
+resource "google_compute_subnetwork" "group2" {
+  name                     = "${var.network_prefix}-group2"
+  ip_cidr_range            = "10.127.0.0/20"
+  network                  = google_compute_network.default.self_link
+  region                   = var.group2_region
+  private_ip_google_access = true
+}
+
+#### CLOUD ROUTER
+# Router and Cloud NAT are required for installing packages from repos (apache, php etc)
+resource "google_compute_router" "group2" {
+  name    = "${var.network_prefix}-gw-group2"
+  network = google_compute_network.default.self_link
+  region  = var.group2_region
+}
+
+#### CLOUD NAT
+module "cloud-nat-group2" {
+  source     = "terraform-google-modules/cloud-nat/google"
+  version    = "~> 5.0"
+  router     = google_compute_router.group2.name
+  project_id = var.project
+  region     = var.group2_region
+  name       = "${var.network_prefix}-cloud-nat-group2"
+}
+
+
+#### EXTERNAL LOAD BALANCER HTTP
+# [START cloudloadbalancing_ext_http_gce]
+module "gce-lb-http" {
+  source  = "terraform-google-modules/lb-http/google"
+  version = "~> 10.0"
+  name    = var.network_prefix
+  project = var.project
+  target_tags = [
+    "${var.network_prefix}-group1",
+    module.cloud-nat-group1.router_name,
+    "${var.network_prefix}-group2",
+    module.cloud-nat-group2.router_name
+  ]
+  firewall_networks = [google_compute_network.default.name]
+
+  backends = {
+    default = {
+
+      protocol    = "HTTP"
+      port        = 80
+      port_name   = "http"
+      timeout_sec = 10
+      enable_cdn  = false
+
+      health_check = {
+        request_path = "/"
+        port         = 80
+      }
+
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
+
+      groups = [
+        {
+          group = module.mig1.instance_group
+        },
+        {
+          group = module.mig2.instance_group
+        },
+      ]
+
+      iap_config = {
+        enable = false
+      }
+    }
+  }
+}
+# [END cloudloadbalancing_ext_http_gce]
+
+
+#### VPC NETWORK
+#creating Network were we are going to deploy all the resources
+resource "google_compute_network" "default" {
+  name                    = var.network_prefix
+  auto_create_subnetworks = "false" #we only want 2 subents therefore we will create them manually
+}
